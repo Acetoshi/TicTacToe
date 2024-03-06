2f0c3c196565858777fd7959b92e28de743b86d6
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-03-06 22:16:31 +0100

    game functionnal, but after the first move, user can misclick and break the page. Also it goes in infinity loop if the game ends. need to make a win function

--- scripts.js
+++ scripts.js
@@ -12,24 +12,60 @@ const cells = [
     document.getElementById("000000001")
 ]
 
+//This will be used to abort all event listeners after the player has clicked on his cell.
+const controller = new AbortController();
+console.log(controller)
+// the gameboard will be represented by an array of length 9, containing '', 'X' or 'O' 
+let gameBoard = ['', '', '', '', '', '', '', '', '']
 
 
 clearAllCells();
 
 //initialise all cells and make them clickable
-for (i=0; i<9; i++){
-    const targetCell=cells[i] // Apparently needed to pass the element into the addEventListener. I wonder why ? 
-    cells[i].addEventListener("click", function() {
-        putSymbol(targetCell,'x');  // Breaks if cells[i] is used instead of targetCells
-    });
+for (i = 0; i < 9; i++) {
+    cells[i].addEventListener("click", putSymbol(i, 'x'), { signal: controller.signal });
 };
 
 
-function putSymbol(targetCell,symbol){
-    targetCell.innerHTML = symbol;
-    console.log('putting '+symbol+' in cell'+String(targetCell))
+function putSymbol(i, symbol) {
+    return function putSymbolCallback() {
+        cells[i].innerHTML = symbol;
+        console.log('putting ' + symbol + ' in cell ' + String(i))
+        controller.abort();
+        makeMove(i);
+    };
 }
 
-function clearAllCells () {
 
+
+function clearAllCells() {
+
+}
+
+function makeMove(lastMoveIndex) {
+
+    gameBoard[lastMoveIndex] = 'X'
+
+    // Check if player has won. 
+
+
+    // DUMB MODE : the computer plays at random 
+
+    let moveIndex = Math.floor(Math.random() * 8);
+    while (gameBoard[moveIndex] != '') {
+        moveIndex = Math.floor(Math.random() * 8);
+    }
+    cells[moveIndex].innerHTML = 'o';
+    gameBoard[moveIndex] = 'O'
+
+
+    console.log(gameBoard);
+    console.log(controller)
+    // make only free cells clickable
+    for (i = 0; i < 9; i++) {
+        if (gameBoard[i] == '') {
+            cells[i].addEventListener("click", putSymbol(i, 'x'));  /// Problem : abort controller can not be re-used. 
+            console.log(i);
+        }
+    }
 }
